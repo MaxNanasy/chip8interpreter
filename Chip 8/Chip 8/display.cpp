f04a70f74b9c46f1ea20a6d5d82a3662d41fb7d6
aaron.peschel <aaron.peschel@b9c7da4e-ea8f-11de-a700-c3e9af1e83fa>
2010-01-23 00:30:36 +0000

    Took out the run method from display. The CPU is now responsible for running the program.

--- Chip 8/Chip 8/display.cpp
+++ Chip 8/Chip 8/display.cpp
@@ -23,73 +23,19 @@ void Display::clear()
 	SDL_FillRect(screen, NULL, 0x000000);
 }
 
+void Display::update()
+{
+	SDL_UpdateRect(screen, 0, 0, WIN_W, WIN_H);
+}
+
 Uint32 Display::toggle_pixel(int pos_x, int pos_y)
 {
 	int x = pos_x * STEP_X;
 	int y = pos_y * STEP_Y;
-
 	SDL_Rect square = { x, y, STEP_X, STEP_Y };
+
 	Uint32 color = pixels[x + y * pitch_px] ^ 0xffffff;
 	SDL_FillRect(screen, &square, color);
+
 	return color;
-}
-
-int Display::run()
-{
-	SDL_Event event;
-
-	while (1)
-	{
-		render();
-
-		while(SDL_PollEvent(&event))
-		{
-			switch(event.type)
-			{
-			case SDL_QUIT:
-				return 0;
-			}
-		}
-	}
-}
-
-void Display::render()
-{
-	if (SDL_MUSTLOCK(screen))
-	{
-		if (SDL_LockSurface(screen) < 0)
-		{
-			return;
-		}
-	}
-
-	// START TEST CODE
-	int i, j;
-	Uint8 buf;
-	Uint8 mem[] = {0xff, 0xaf, 0x87, 0x13 };
-	int I_addr = 0;
-	static int X = 0;
-	static int Y = 0;
-	int N = 4;
-
-	for (i = 0; i < N; i++)
-	{
-		buf = mem[I_addr + i];
-
-		for (j = 0; j < sizeof(buf)*8; j++)
-		{
-			if (buf & 0x80)
-				toggle_pixel(X + j, Y + i);
-
-			buf <<= 1;
-		}
-	}
-	// END TEST CODE
-
-	if (SDL_MUSTLOCK(screen))
-	{
-		SDL_UnlockSurface(screen);
-	}
-
-	SDL_UpdateRect(screen, 0, 0, WIN_W, WIN_H);
 }
