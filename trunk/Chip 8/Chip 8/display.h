1ae04f685dd49d54062c7098a876b185577e3fe2
aaron.peschel <aaron.peschel@b9c7da4e-ea8f-11de-a700-c3e9af1e83fa>
2010-01-23 01:17:53 +0000

    Added copyright information to source files.

--- trunk/Chip 8/Chip 8/display.h
+++ trunk/Chip 8/Chip 8/display.h
@@ -1,3 +1,21 @@
+//    This program is a Chip-8 interpreter. It takes Chip-8 programs and runs them.
+//    Copyright (C) 2010  Aaron Peschel
+//
+//    This file is part of Chip-8 Interpreter.
+//
+//    Chip-8 Interpreter is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Chip-8 Interpreter is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Chip-8 Interpreter.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
